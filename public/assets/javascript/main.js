9765e72530dfd9b60acbc098d5154ccb4d580dfd
holland cookson <hollandcookson@hollands-MacBook-Pro-2.local>
2018-12-19 11:34:18 -0500

    adding fade-in/styling section 2 content

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	var ourScene = new ScrollMagic.Scene({
 		triggerElement: '#test',
 		duration: '90%',
-		triggerHook: .79
+        triggerHook: .79,
 	})
 	.setClassToggle('#test', 'fade-in') // add class to project01
 	.addIndicators({
@@ -16,9 +16,35 @@ $(document).ready(function(){
 		colorStart: '#75C695',
 		colorEnd: 'pink'
 	}) // this requires a plugin
+	.addTo(controller);
+
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '#test-2',
+		duration: '90%',
+        triggerHook: .79,
+	})
+	.setClassToggle('#test-2', 'fade-in') // add class to project01
+	.addIndicators({
+		name: 'fade scene',
+		colorTrigger: 'black',
+		colorStart: '#75C695',
+		colorEnd: 'pink'
+	}) // this requires a plugin
+	.addTo(controller);
+	var ourScene = new ScrollMagic.Scene({
+		triggerElement: '#test-3',
+		duration: '90%',
+        triggerHook: .79,
+	})
+	.setClassToggle('#test-3', 'fade-in') // add class to project01
+	.addIndicators({
+		name: 'fade scene',
+		colorTrigger: 'black',
+		colorStart: '#75C695',
+		colorEnd: 'pink'
+	}) // this requires a plugin
     .addTo(controller);
     
-
 });
 
 
